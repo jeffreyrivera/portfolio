e27314bc0c93ee29e402cb3642745af5cc69c69c
Jeffrey Rivera <jeffreyrivera@gmail.com>
2021-03-11 12:41:41 -0800

    initial commit

---
+++ src/components/AlgoApp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class AlgoApp extends React.Component {
+
+    
+    render() {
+        return (
+            <div>
+                My algo App
+            </div>
+        );
+    }
+}
+
+
+export default AlgoApp;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AlgoApp from '/components/AlgoApp';
+
+ReactDOM.render(<AlgoApp />, document.querySelector('#root'));
+
+
+
