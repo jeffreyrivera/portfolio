f69ae228e044c68b62f2c303c61c82d4fa8c3b09
Jeffrey Rivera <jeffreyrivera@gmail.com>
2021-03-13 17:09:26 -0800

    added Code Editor scaffolding to develop on the fly questions answer prototype

---
+++ src/components/CodeEditor.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+class CodeEditor extends React.Component {
+
+
+    render () {
+        return (
+            <div>
+                <h1>Code Editor</h1>
+
+
+            </div>
+        );
+    }
+}
+
+
+export default CodeEditor;
