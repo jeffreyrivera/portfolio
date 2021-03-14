47a039a9ae563ee19b20d590945ebb4cc1ec27a1
Jeffrey Rivera <jeffreyrivera@gmail.com>
2021-03-13 17:08:28 -0800

    Figure out the App Layout to display questions and asnwers, HTML in place and mockup COmponents already. Will have them working next commit

--- src/components/AlgoApp.js
+++ src/components/AlgoApp.js
@@ -3,13 +3,26 @@ import React from 'react';
 import QuestionsMenu from './QuestionsMenu';
 import ViewMenu from './ViewMenu';
 import PrismCode from './PrismCode';
+import CodeEditor from './CodeEditor';
 
 
 const code = `
 def fib_iterative(n):
     a, b = 0, 1
     while n > 0:
-        a, b = b, a + b
+        a, b = b, a + b sdasdsadasdasdasdsdasdadas
+        n -= 1
+    return a
+
+    a, b = 0, 1
+    while n > 0:
+        a, b = b, a + b sdasdsadasdasdasdsdasdadas
+        n -= 1
+    return a
+
+    a, b = 0, 1
+    while n > 0:
+        a, b = b, a + b sdasdsadasdasdasdsdasdadas
         n -= 1
     return a
 `
@@ -28,23 +41,6 @@ class AlgoApp extends React.Component {
         const questionsArray = [
             {question_id: 1, 
                 title: "Fibonacci Series", description: "You are required to return the nth element (n) and print it out to the console from Fibonacci series.",
-                answers: [
-                    { 
-                        answerId: 1,
-                        text: 'fsdfasdfsdf',
-                        code: ''
-                    },
-                    { 
-                        answerId: 2,
-                        text: 'fsdfasdfsdf',
-                        code: ''
-                    },
-                    { 
-                        answerId: 3,
-                        text: 'fsdfasdfsdf',
-                        code: ''
-                    },
-                ]
             },
             {question_id: 2, questionTitle: "Second Question", description: "What are Design Patterns Seriously ?"},
             {question_id: 3, questionTitle: "Thirst Question", description: "What are Design Patterns No Way?"}
@@ -78,7 +74,7 @@ class AlgoApp extends React.Component {
     render() {
         return (
             <div>
-                <div className="ui segment">
+                <div className="ui container">
                     <QuestionsMenu 
                         categories = {this.state.categories}
                         current = {this.state.categorySelected}
@@ -90,11 +86,74 @@ class AlgoApp extends React.Component {
                         onViewSelect = {this.onViewSelect}
                     />
                 </div>
-                <div className="ui segment">
-                    <PrismCode
-                        code={code}
-                        language='python'
-                    />
+                <div class="ui container">
+                    <div class="ui stackable two column grid">
+                        <div class="twelve wide two column grid">
+                            
+                            <div class="ui segment">
+                                <h3>Title</h3> 
+                                <p>It doesn't break on MSIE6 or other legacy browsers. 
+                                    It either uses a fall-back or simply does nothing.</p>   
+                            </div>
+                            
+                            <div class="ui stackable doubling two column grid">
+                                <div class="eleven wide column">
+                                    <div class="ui segment">
+                                        <PrismCode
+                                            code={code}
+                                            language='python'
+                                        />
+                                    </div>
+                                </div>
+                                <div class="five wide column">
+                                    <div class="ui segment">
+                                        <h5>Solutions</h5>
+                                        <p><b>Array</b>  > Greedy</p>
+                                        
+                                        <p>DOMPurify is written in JavaScript and works in 
+                                            all modern browsers (Safari (10+), Opera (15+), 
+                                            Internet Explorer (10+), Edge, Firefox and Chrome - 
+                                            as well as almost anything else using Blink or WebKit).</p>
+                                        <p>It doesn't break on MSIE6 or other legacy browsers. 
+                                        It either uses a fall-back or simply does nothing.</p>
+                                    </div>
+                                </div>
+                                <div className="column">
+                                    <div class="ui segment">
+                                        Array ---   DP  --- Medium
+                                    </div>
+                                </div>
+                            </div>
+
+                            
+                        </div>
+                        <div class="four wide column">
+                            <div class="ui segment">
+                                Content
+                                Content
+                                Content
+                                Content
+                                Content
+                                ContentContent
+                                Content
+                                Content
+                                Content
+                                Content
+                                Content
+                                ContentContent
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+
+                <div className="ui container">
+                    <div class="ui stackable two column grid">
+                        <div class="column">
+                            <CodeEditor />
+                        </div>
+                    </div>
                 </div>
             </div>
         );
